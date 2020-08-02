7fee3e1715f1eab894ffbd26ce03f8b5195e6525
kousuke_ida <takashiii1192@gmail.com>
2020-08-02 13:26:13 +0900

    fix activated? bug

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -2,10 +2,9 @@
 
 class AccountActivationsController < ApplicationController
   def edit
-    p params
     user = User.find_by(email: params[:email])
 
-    if user&.activated? && user&.authenticated?(:activation, params[:id])
+    if user && !user.activated? && user.authenticated?(:activation, params[:id])
       user.update_attribute(:activated, true)
       user.update_attribute(:activated_at, Time.zone.now)
       redirect_to 'https://takashiii-hq.com'
