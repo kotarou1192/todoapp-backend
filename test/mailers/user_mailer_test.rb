a49597904b5389041b8f865e020b27fe7ccb28a0
kousuke_ida <takashiii1192@gmail.com>
2020-08-02 15:32:23 +0900

    update and pass test

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -1,20 +1,29 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
-  test "account_activation" do
-    mail = UserMailer.account_activation
-    assert_equal "Account activation", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+  def setup
+    @user = User.new(id: SecureRandom.uuid,
+                     name: 'hoge',
+                     email: 'hoge@email.com',
+                     password: 'hogefuga',
+                     password_confirmation: 'hogefuga')
+    @user.save
+  end
+  test 'account_activation' do
+    mail = UserMailer.account_activation(@user)
+    assert_equal 'Account activation', mail.subject
+    assert_equal [@user.email], mail.to
+    assert_equal ['noreply@example.com'], mail.from
+    # assert_match 'ようこそ', mail.body.encoded
   end
 
-  test "password_reset" do
+  test 'password_reset' do
     mail = UserMailer.password_reset
-    assert_equal "Password reset", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    assert_equal 'Password reset', mail.subject
+    assert_equal ['to@example.org'], mail.to
+    assert_equal ['noreply@example.com'], mail.from
+    # assert_match 'Hi', mail.body.encoded
   end
-
 end
