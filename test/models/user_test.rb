90421ac3a090fd9638b9105f64e5f69b21f77590
kousuke_ida <takashiii1192@gmail.com>
2020-08-05 05:49:17 +0900

    add tests for user CRUD

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,12 +2,16 @@
 
 require 'test_helper'
 
-class UserTest < ActiveSupport::TestCase
+class UserTest < ActionDispatch::IntegrationTest
   def setup
+    @email = 'hoge@email.com'
     @user = User.new(id: SecureRandom.uuid,
                      name: 'hoge',
-                     email: 'hoge@email.com',
+                     email: @email,
                      password: 'hogefuga')
+    @user.password_digest = User.digest(@user.password)
+    @user.save
+    @user.update(activated: true)
   end
 
   test 'should be valid' do
@@ -22,5 +26,36 @@ class UserTest < ActiveSupport::TestCase
   test 'password should have a minimum length' do
     @user.password = 'a' * 5
     assert_not @user.valid?
+  end
+
+  test 'user should create' do
+    user_name = 'hogefuga'
+    user_email = 'hogefuga@hoge.com'
+    post '/users/create', params: { name: user_name, email: user_email, password: 'hogefuga' }
+    user = User.find_by(email: 'hogefuga@hoge.com')
+    assert user.name == user_name
+  end
+
+  test 'user should be login' do
+    post '/users/login', params: { email: @user.email, password: @user.password }
+    assert Session.find_by(user_email: @email)
+  end
+
+  test 'user should be logout' do
+    post '/users/login', params: { email: @user.email, password: @user.password }
+
+    body = JSON.parse(response.body)
+    token = body['token']
+    post '/users/logout', params: { token: token }
+    assert_not Session.find_by(user_email: @user.email)
+  end
+
+  test 'should delete user' do
+    post '/users/login', params: { email: @user.email, password: @user.password }
+
+    body = JSON.parse(response.body)
+    token = body['token']
+    post '/users/delete', params: { token: token, email: @email }
+    assert_not User.find_by(email: @email)
   end
 end
